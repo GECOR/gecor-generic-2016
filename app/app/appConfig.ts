f02dbe7e32214bcc4ab2bf5203a35bba44528394
uiktiomasfeliz <uiktiomasfeliz@gmail.com>
2016-03-31 10:38:22 +0200

    Use ip for service

--- app/app/appConfig.ts
+++ app/app/appConfig.ts
@@ -1,2 +1,2 @@
-export var urlGecorApi = "http://localhost/GecorApi/api/";
-//export var urlGecorApi = "http://192.168.1.147/GecorApi/api/";
+//export var urlGecorApi = "http://localhost/GecorApi/api/";
+export var urlGecorApi = "http://192.168.1.147/GecorApi/api/";

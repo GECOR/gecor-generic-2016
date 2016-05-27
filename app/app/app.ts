f615b445d80bafaab96541468f00b33af61926a0
uiktiomasfeliz <uiktiomasfeliz@gmail.com>
2016-05-27 14:21:18 +0200

    Add multiple language support

--- app/app/app.ts
+++ app/app/app.ts
@@ -1,27 +1,41 @@
-import {ViewChild, Type} from '@angular/core';
+import {ViewChild, Type, provide} from '@angular/core';
+import {Http} from '@angular/http';
 import {App, Platform, Storage, SqlStorage} from 'ionic-angular';
-import {Splashscreen} from 'ionic-native';
+import {Splashscreen, Globalization} from 'ionic-native';
+import {TranslateService, TranslateLoader, TranslateStaticLoader, TranslatePipe} from 'ng2-translate/ng2-translate';
 import {ConferenceData} from './providers/conference-data';
 import {GeolocationProvider} from './providers/geolocation';
 import {UserData} from './providers/user-data';
 import {SlidePage} from './pages/slides/slide';
 import {TabsPage} from './pages/tabs/tabs';
+import {defaultLanguage, folderLanguage, sourceLanguage, compareLanguage} from './appConfig';
 // https://angular.io/docs/ts/latest/api/core/Type-interface.html
 
 @App({
-  templateUrl: './build/app.html',
-  providers: [ConferenceData, UserData, GeolocationProvider],
-  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
+  templateUrl: './build/app.html',  
+  config: {}, // http://ionicframework.com/docs/v2/api/config/Config/
+  providers: [ConferenceData
+              , UserData
+              , GeolocationProvider
+              , provide(TranslateLoader, {
+                useFactory: (http: Http) => new TranslateStaticLoader(http, folderLanguage, sourceLanguage),
+                deps: [Http]
+              }),
+              TranslateService
+              ],
+    pipes: [TranslatePipe]
 })
 export class MyApp {
   storage: any;
+  translate: any;
   user: any;
   rootPage: Type;// = SlidePage;
 
-  constructor(platform: Platform, confData: ConferenceData, geo: GeolocationProvider) {
+  constructor(platform: Platform, confData: ConferenceData, geo: GeolocationProvider, translate: TranslateService) {
     // load the conference data
     confData.load();    
     this.storage = new Storage(SqlStorage);
+    this.translate = translate;
     
     this.storage.get('user').then((user) => {
       if(user){        
@@ -36,14 +50,28 @@ export class MyApp {
         }
       }else{
         this.rootPage = SlidePage;
-      }    
+      }  
         
       Splashscreen.hide();           
     });
     
-     platform.ready().then(() => {
-        
-      });
-      
+    Globalization.getPreferredLanguage().then((obj) =>{//get device language
+      console.log(obj.value);
+      this.initializeTranslateServiceConfig(obj.value.split('-')[0]);//initialize sending lowercase language
+    }, (err)=>{
+      console.log(err); 
+      this.initializeTranslateServiceConfig(defaultLanguage);//initialize sending lowercase language default 
+    });
+    
+    //this.initializeTranslateServiceConfig();
+    
+    platform.ready().then(() => {});
   }
+  
+  initializeTranslateServiceConfig(lang) {
+    //var userLang = compareLanguage.test(lang) ? lang : defaultLanguage;     
+    this.translate.setDefaultLang(defaultLanguage);   
+    this.translate.use(compareLanguage.test(lang) ? lang : defaultLanguage);
+  }
+  
 }
--- app/app/appConfig.ts
+++ app/app/appConfig.ts
@@ -1,2 +1,8 @@
 //export var urlGecorApi = "http://localhost/GecorApi/api/";
 export var urlGecorApi = "http://192.168.1.147/GecorApi/api/";
+
+//app.ts -- config multilanguages
+export let defaultLanguage = 'en';
+export let folderLanguage = 'assets/i18n';
+export let sourceLanguage = '.json';
+export let compareLanguage = /(es|en)/gi;
